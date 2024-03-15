4e1b06d8f61cbd32dc332519c3e83feed6dc520b
Adetola-Adeya <detolaadeya@gmail.com>
2024-03-16 00:23:03 +0100

    Test commit

---
+++ my_project/testcode.py
@@ -0,0 +1,9 @@
+import your_code
+
+def test_add():
+    assert your_code.add(1, 2) == 3
+    assert your_code.add(5, 5) == 10
+
+def test_subtract():
+    assert your_code.subtract(5, 3) == 2
+    assert your_code.subtract(10, 7) == 3
